f9704aa02dd554455a32840c94977980627203f3
JohanVillalasso <113487194+JohanVillalasso@users.noreply.github.com>
2023-07-15 19:38:28 -0500

    hola mundo johan
    hola mundo de Johan

--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 def hola_mundo(request):
-    context={}
+    nombre = "Johan Esteban"
+    apellido = "Villarreal Lasso"
+    telefono = "123456789"
+    context={
+        'nombres' :nombre,
+        'apellidos' :apellido,
+        'telefono' :telefono
+    }
     return render (request, 'index.html', context)
