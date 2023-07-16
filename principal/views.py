16202caae5aca293bd7de4ce1b65cdd4d76d2ceb
Alexander Corredor <alexanderjose31@gmail.com>
2023-07-15 19:37:12 -0500

    Modelo Usuario
    Inicie trabajo con modelo Usuario, todavía no finalizado

--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def hola_mundo(request):
-    context={}
+    nombre = 'Alexander' 
+    apellido = 'Corredor'
+    context={
+      'nombres':nombre,
+      'apellidos': apellido
+    }
     return render (request, 'index.html', context)
