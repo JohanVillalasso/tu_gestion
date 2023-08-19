b9bd822ffb577c249c40ad78120adbf1687bba00
JohanVillalasso <113487194+JohanVillalasso@users.noreply.github.com>
2023-08-19 16:25:07 -0500

    interfaces funcionando

--- principal/urls.py
+++ principal/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from principal.views import hola_mundo
+from principal.views import inicio_sesion, menu_ppal, rutero, cap_guia, seg_cliente, proy_cumpl, eje_acu, form_gcom
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hola_mundo, name='inicio'),
+    path('', inicio_sesion, name='inicio'),
     path('usuarios/',include('usuarios.urls')),
     path('ciudades/',include('ciudades.urls')),
     path('clientes/',include('clientes.urls')),
     path('guia/',include('guia.urls')),
+    path('menu/', menu_ppal, name=('menu-ppal')),
+    path('rutero/', rutero, name=('rutero')),
+    path('cap-guia/', cap_guia, name=('cap-guia')),
+    path('seg-cliente/', seg_cliente, name=('seg-cliente')),
+    path('proy-cumpl/', proy_cumpl, name=('proy-cumpl')),
+    path('eje-acu/', eje_acu, name=('eje-acu')),
+    path('form-gcom/', form_gcom, name=('form-gcom')),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,13 +1,50 @@
 from django.shortcuts import render
 
 
-def hola_mundo(request):
-    nombre = 'Johan' 
-    apellido = 'Villarreal'
-    telefono = '3196424443'
+def inicio_sesion(request):
     context={
-      'nombres':nombre,
-      'apellidos': apellido,
-      'telefono': telefono
+        
     }
     return render (request, 'index.html', context)
+  
+def menu_ppal(request):
+    context={
+    
+    }
+    return render (request, 'main.html', context)
+  
+def rutero(request):
+    context={
+    
+    }
+    return render (request, 'rutero.html', context)
+
+def cap_guia(request):
+    context={
+    
+    }
+    return render (request, 'cap-guia.html', context)
+
+def seg_cliente(request):
+    context={
+    
+    }
+    return render (request, 'seg-cliente.html', context)
+
+def proy_cumpl(request):
+    context={
+    
+    }
+    return render (request, 'proy-cumpl.html', context)
+
+def eje_acu(request):
+    context={
+    
+    }
+    return render (request, 'eje-acu.html', context)
+
+def form_gcom(request):
+    context={
+    
+    }
+    return render (request, 'form-gcom.html', context)
